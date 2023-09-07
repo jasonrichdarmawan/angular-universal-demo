27af6ed17e12e80ad30502f06e83f11145752297
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-09-07 18:55:07 +0700

    chore: lazy loading heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
+// import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
 import { MessagesComponent } from './messages/messages.component';
 
@@ -36,7 +36,7 @@ import { isPlatformBrowser } from '@angular/common';
   declarations: [
     AppComponent,
     DashboardComponent,
-    HeroesComponent,
+    // HeroesComponent,
     HeroDetailComponent,
     MessagesComponent,
     HeroSearchComponent
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    HeroesComponent
+  ],
+  imports: [
+    CommonModule,
+    HeroesRoutingModule
+  ]
+})
+export class HeroesModule { }
