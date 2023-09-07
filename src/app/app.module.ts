0d8bdb8c47b15693884533e2f6642f3532c09f31
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2023-09-07 18:51:58 +0700

    chore: enable noUnusedLocals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -47,8 +47,8 @@ export class AppModule {
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
     @Inject(APP_ID) private appId: string) {
-    const platform = isPlatformBrowser(platformId) ?
+    const platform = isPlatformBrowser(this.platformId) ?
       'in the browser' : 'on the server';
-    console.log(`Running ${platform} with appId=${appId}`);
+    console.log(`Running ${platform} with appId=${this.appId}`);
   }
 }
